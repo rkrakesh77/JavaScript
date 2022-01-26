ec368009f12677f3c790e5bc45ff080759809394
Rakesh Kumar <rkrakeshkumar771996@gmail.com>
2022-01-26 15:31:16 +0530

    First

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("test");
+//console.log("test");
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arr.filter((a) => a % 2 == 0));
+
+arr.forEach((a, i) => {
+    console.log(a, i);
+})
